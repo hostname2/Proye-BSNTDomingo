22965bee7ba9fde7f2c869ec4c49a1dd6ab6f09e
Roberto Rojas <roberto.2rojas@gmail.com>
2020-09-23 19:07:37 -0600

    Servlets

--- SIGB/src/Sevicios/SevletEliminarDocumento.java
+++ SIGB/src/Sevicios/SevletEliminarDocumento.java
@@ -1,8 +1,12 @@
-
 package Sevicios;
 
 import SIGB.Modelo.Dao.GestorDao_Documento;
+import SIGB.Modelo.Entidades.Persona;
+import SIGB.Modelo.Entidades.Usuario;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import javax.servlet.ServletException;
@@ -10,15 +14,30 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class SevletEliminarDocumento extends HttpServlet{
-    
+public class SevletEliminarDocumento extends HttpServlet {
+
     private void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         response.setContentType("text/html;charset=UTF-8");
+        String destino = "registroAceptado.jsp";
 
-        String destino = "";
+        int sl_numero = Integer.parseInt(request.getParameter("idDocumento"));
 
-            
+        if (Formulario.validacion(request, response)) {
+
+            try {
+                if (pf_sDD.eliminarDocumento(sl_numero)) {
+                    destino = "exito.jsp?exito=2";
+                } else {
+                    destino = "error.jsp?error=4";
+                }
+            } catch (Exception ex) {
+            }
+
+        } else {
+            destino = "error.jsp?error=24";
+        }
+
         response.sendRedirect(destino);
     }
 
@@ -37,5 +56,5 @@ public class SevletEliminarDocumento extends HttpServlet{
     }
 
     private final GestorDao_Documento pf_sDD = GestorDao_Documento.getInstancia();
-    
+
 }
